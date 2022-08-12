b9ff53daa349b30cdfa89017570bfc4edb5e135e
Chanwoo <tree698@gmail.com>
2022-08-13 00:01:47 +0900

    refactoring for javaScript

--- src/main.js
+++ src/main.js
@@ -1,71 +1,46 @@
 'use strict';
 
-const items = document.querySelector('.items');
-const app = document.querySelector('.app');
-
 loadItems() //
   .then((items) => {
     displayItems(items);
     setEventListener(items);
-  })
-  .catch((error) => alert('Items are not loaded or Network Error!'));
+  });
 
 async function loadItems() {
-  const response = await fetch('../Data/data.json');
+  const response = await fetch('../data/data.json');
   const json = await response.json();
   const items = json.items;
   return items;
 }
 
 function displayItems(items) {
-  items.map((item) => {
-    addItem(createItem(item.image, item.gender, item.size));
-  });
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
 
-function addItem(item) {
-  items.appendChild(item);
-}
-
-function createItem(img, gender, size) {
-  const item = document.createElement('li');
-  item.setAttribute('class', 'item');
-  item.innerHTML = `
-      <img src=${img} alt="item image" class="item__thumbnail" />
-      <span class="item__description">${gender}, ${size}</span>`;
-  return item;
+function createHTMLString(item) {
+  return `<li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+            <p class="item__description">${item.gender} ${item.size}</p>
+          </li>`;
 }
 
 function setEventListener(items) {
-  app.addEventListener('click', (event) => {
-    deleteItems();
-    switch (event.target.className) {
-      case 'menu__img tshirt':
-        return displaySelectedItems(items, 'type', 'tshirt');
-      case 'menu__img pants':
-        return displaySelectedItems(items, 'type', 'pants');
-      case 'menu__img skirt':
-        return displaySelectedItems(items, 'type', 'skirt');
-      case 'button blue':
-        return displaySelectedItems(items, 'color', 'blue');
-      case 'button yellow':
-        return displaySelectedItems(items, 'color', 'yellow');
-      case 'button pink':
-        return displaySelectedItems(items, 'color', 'pink');
-      case 'logo':
-        return displayItems(items);
-      default:
-        throw new Error('error');
-    }
-  });
+  const log = document.querySelector('.logo');
+  const nav = document.querySelector('.nav');
+
+  log.addEventListener('click', () => displayItems(items));
+  nav.addEventListener('click', (event) => onButtonClick(event, items));
 }
 
-function deleteItems() {
-  const toBeDeleted = document.querySelectorAll('.item');
-  toBeDeleted.forEach((item) => item.remove());
-}
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
 
-function displaySelectedItems(items, key, itemValue) {
-  const selectedItems = items.filter((item) => item[key] === itemValue);
-  displayItems(selectedItems);
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter((item) => item[key] === value));
 }
